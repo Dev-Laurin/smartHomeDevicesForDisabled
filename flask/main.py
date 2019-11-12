a4b0e5c728b77db7ce8b9f89bfe80970be94edaa
Laurin <lafisher@alaska.edu>
2019-11-11 22:21:56 -0900

    Working on listing devices by home category and displaying based on input type.

--- flask/main.py
+++ flask/main.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import (Flask, render_template, request, 
+	url_for, redirect, flash)
 from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test2.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test3.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
 db = SQLAlchemy(app)
 
@@ -15,31 +16,6 @@ db = SQLAlchemy(app)
 
 # 	def __repr__(self):
 # 		return '<User %r>' % self.username 
-
-class disabilitycategory(db.Model):
-	def __str__(self):
-		return self.name 
-
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80), nullable=False)
-	disabilities = db.relationship('Disability',
-		backref='disabilitycategory', lazy=True)
-	device = db.relationship('Device',
-		backref='disabilitycategory', lazy=True)
-
-class Disability(db.Model):
-	def __str__(self):
-		return self.name 
-
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(80), nullable=False)
-	category_id = db.Column(db.Integer, db.ForeignKey('disabilitycategory.id'),
-		nullable=False)
-	# ATR = db.relationship('assistivetechrating', 
-	# 	backref='disability')
-
-	community_rating = db.relationship('communityrating', 
-		backref='disability')
 
 class devicecategory(db.Model):
 	def __str__(self):
@@ -56,21 +32,6 @@ class devicecategory(db.Model):
 
 # 	id = db.Column(db.Integer, primary_key=True)
 	
-
-class communityrating(db.Model):
-	def __str__(self):
-		return  self.name 
-
-	id = db.Column(db.Integer, primary_key=True)
-	description = db.Column(db.String(500), nullable=False)
-	title = db.Column(db.String(50), nullable=False)
-	rating = db.Column(db.Integer, nullable=False)
-	disability_id = db.Column(db.Integer, 
-		db.ForeignKey('disability.id'), 
-		nullable=False)
-	device = db.relationship('Device',
-		backref='communityrating', lazy=True)
-
 class paymentoccurence(db.Model):
 	def __str__(self):
 		return  self.name 
@@ -79,6 +40,17 @@ class paymentoccurence(db.Model):
 	name = db.Column(db.String(20), nullable=False)
 	device = db.relationship('Device', 
 		backref='paymentoccurence', lazy=True)
+
+homecategories = db.Table('homecategories',
+	db.Column('homecategory_id', db.Integer, db.ForeignKey('homecategory.id'), primary_key=True),
+	db.Column('device_id', db.Integer, db.ForeignKey('device.id'), primary_key=True))
+
+class homecategory(db.Model): 
+	def __str__(self):
+		return self.name 
+
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(90), nullable=False)
 
 class Device(db.Model):
 	def __str__(self):
@@ -97,55 +69,44 @@ class Device(db.Model):
 	category_id = db.Column(db.Integer, 
 		db.ForeignKey('devicecategory.id'),
 		nullable=False)
+	homecategories = db.relationship('homecategory', 
+		secondary=homecategories, lazy='subquery',
+		backref=db.backref('devices', lazy=True))
 	# atp_rating_id = db.Column(db.Integer, 
 	# 	db.ForeignKey('assistivetechrating.id'),
 	# 	nullable=False)
-	community_rating_id = db.Column(db.Integer, 
-		db.ForeignKey('communityrating.id'))
 
-	disability_category_id = db.Column(db.Integer, 
-		db.ForeignKey('disabilitycategory.id'), 
-		nullable=False)
 	effectiveness_rating = db.Column(db.Integer, nullable=False)
-	relevance_rating = db.Column(db.Integer, nullable=False)
 	narrative = db.Column(db.String(500), nullable=False)
-	# device = db.relationship('Device', 
-	# 	backref='assistivetechrating', lazy=True)
 
-##Routing 
-@app.route('/')
-def main(name=None):
-	disCat = disabilitycategory.query.all()
-	return render_template('form.html', disCat=disCat)
-
-@app.route('/listDevices', methods=["GET", "POST"])
+#Routing 
+@app.route('/', methods=["GET", "POST"])
 def list(name=None):
-	devices = Device.query.join(disabilitycategory, 
-			paymentoccurence, devicecategory).all()
+	categories = devicecategory.query.all()
+	devices = Device.query.all() 
+	homecategories = homecategory.query.all()
+
+	#Refiltering devices
 	if request.method == 'POST':
-	#find all the compatible devices
-		devices = []
-		for dis in request.form:
-			disability = disabilitycategory.query.filter_by(name=dis).first()
-			print(disability.name)
-			print(disability.id)
-			ds = Device.query.join(disabilitycategory, 
-				paymentoccurence, 
-				devicecategory).filter(disabilitycategory.id==disability.id).all()
-			for d in ds:
-				print(d)
-				devices.append(d)
-	return render_template('list.html', devices=devices)
+		#find all the compatible devices
+		
+		deviceCats = request.form.getlist('inputCat[]')
+		if deviceCats: 
+			devices = []
+		for dc in deviceCats: 
+			deviceCat = devicecategory.query.filter_by(name=dc).first()
+			devices.extend(Device.query.filter_by(category_id=deviceCat.id).all())
+
+	return render_template('list.html', devices=devices, 
+		categories=categories, homecategories=homecategories)
 
 @app.route('/createDevice', methods=["GET", "POST"])
 def createDevice(name=None):
 	po = paymentoccurence.query.all()
 	dc = devicecategory.query.all()
-	disC = disabilitycategory.query.all()
 	if request.method == 'POST':
 		po = paymentoccurence.query.filter_by(name=request.form.get('payment_occurence')).first()
 		dc = devicecategory.query.filter_by(name=request.form.get('device_category')).first()
-		disC = disabilitycategory.query.filter_by(name=request.form.get('disability_category')).first()
 		db.session.add(Device(name=request.form.get('name'), 
 			description=request.form.get('desc'), 
 			price=request.form.get('price'),
@@ -153,14 +114,39 @@ def createDevice(name=None):
 			payment_occurence_id=po.id, 
 			link=request.form.get('link'),
 			category_id=dc.id, 
-			disability_category_id=disC.id, 
 			effectiveness_rating=request.form.get('effectiveness_rating'),
-			relevance_rating=request.form.get('relevance_rating'),
 			narrative=request.form.get('narrative')
 			))
 		db.session.commit()
 		return redirect(url_for('list'))
-	return render_template('create_device.html', po=po, deviceCat=dc, disabilityCat=disC)
+	return render_template('create_device.html', po=po, deviceCat=dc)
+
+@app.route('/showDevices', methods=["POST"])
+def showDeviceOnCategory(name=None):
+	#get devices with compatible home categories
+	print(request.form)
+	print(request.form.get('homecategory')) 
+	homeCat = homecategory.query.filter_by(name=request.form.get('homecategory')).first()
+	print(homeCat)
+	devices = Device.query.all() 
+	deviceCats = request.form.getlist('inputCat[]')
+	print(deviceCats)
+	if deviceCats: 
+		devices = []
+	for dc in deviceCats: 
+		deviceCat = devicecategory.query.filter_by(name=dc).first()
+		devices.extend(Device.query.join(devicecategory,
+			homecategories).filter(
+			category_id=deviceCat.id).filter(
+			homecategory_id==homeCat).all())
+
+	return render_template('list_devices_by_category.html', 
+		devices=devices, category=homeCat.name)
+
+@app.route('/editCategories')
+def editCategories(name=None):
+	deviceCat = devicecategory.query.join(Device).filter(Device.category_id==devicecategory.id).all()
+	return render_template('categories.html', deviceCat=deviceCat)
 
 if __name__ == '__main__':
 	app.run()
